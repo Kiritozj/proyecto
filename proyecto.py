b0d1a88b0cd75d8b59d125284eb6f696539f2ffa
Kiritozj <jhorian.arce@correounivalle.edu.co>
2024-06-02 18:53:24 -0500

    interfaz

--- proyecto.py
+++ proyecto.py
@@ -1,6 +1,7 @@
 #Reino del Dragon....
 import random
 import time
+import tkinter as tk
 
 lista1 = []
 lista2 = []
@@ -8,51 +9,74 @@ lista3 = []
 lista4 = []
 lista5 = []
 
-def introduccion():
-    print ("Estamos en una tierra llena de dragones. Delante de nuestro,")
-    print ("se ven dos cuevas. En una cueva, el dragon es amigable")
-    print ("y compartira el tesoro contigo. El otro dragon")
-    print ("es codicioso y hambriento, y te va a comer ni bien te vea.")
-    print ("")
 
-def CambiarCueva():
-    cueva = ""
-    while cueva != "1" and cueva != "2":
-        print ("Ha que cueva quieres entrar? 1 o 2?")
-        cueva = input()
+app = tk.Tk()
+
+app.geometry("600x300")
+
+app.configure(background="black")
+
+tk.Wm.wm_title(app, "Funciones al ataque")
+
+
+
+
+app.mainloop()
+
+
+
+
+
+
+
+
+
+
+# def introduccion():
+#     print ("Estamos en una tierra llena de dragones. Delante de nuestro,")
+#     print ("se ven dos cuevas. En una cueva, el dragon es amigable")
+#     print ("y compartira el tesoro contigo. El otro dragon")
+#     print ("es codicioso y hambriento, y te va a comer ni bien te vea.")
+#     print ("")
+
+# def CambiarCueva():
+#     cueva = ""
+#     while cueva != "1" and cueva != "2":
+#         print ("Ha que cueva quieres entrar? 1 o 2?")
+#         cueva = input()
        
-    return cueva
-def cheqcueva(CambiarCueva):
-    print ("Te acercas a la Cueva...")
-    time.sleep(2)
-    print ("Esta oscuro y tenebroso...")
-    time.sleep(2)
-    print ("Un gran dragon salta delante tuyo, abre su boca y...")
-    print ("")
-    time.sleep(2)
+#     return cueva
+# def cheqcueva(CambiarCueva):
+#     print ("Te acercas a la Cueva...")
+#     time.sleep(2)
+#     print ("Esta oscuro y tenebroso...")
+#     time.sleep(2)
+#     print ("Un gran dragon salta delante tuyo, abre su boca y...")
+#     print ("")
+#     time.sleep(2)
    
-    FriendlyCueva = random.randint (1, 2)
+#     FriendlyCueva = random.randint (1, 2)
    
-    if CambiarCueva == str(FriendlyCueva):
-        print ("Te entrega el tesoro...")
-    else:
-        print ("El dragon te come de un bocado....")
+#     if CambiarCueva == str(FriendlyCueva):
+#         print ("Te entrega el tesoro...")
+#     else:
+#         print ("El dragon te come de un bocado....")
    
-EmpezarNuevo = ("si")
+# EmpezarNuevo = ("si")
 
-while EmpezarNuevo == ("s") or EmpezarNuevo == ("si"):
-        introduccion()
+# while EmpezarNuevo == ("s") or EmpezarNuevo == ("si"):
+#         introduccion()
    
-        NumCaverna = CambiarCueva()
+#         NumCaverna = CambiarCueva()
    
-        cheqcueva(NumCaverna)
+#         cheqcueva(NumCaverna)
    
-        print ("Quieres jugar de nuevo? (si o no)")
-        EmpezarNuevo = input()
+#         print ("Quieres jugar de nuevo? (si o no)")
+#         EmpezarNuevo = input()
 
-if __name__=="__main__":
-    lista1 = []
-    lista2 = []
-    lista3 = []
-    lista4 = []
-    lista5 = []
+# if __name__=="__main__":
+#     lista1 = []
+#     lista2 = []
+#     lista3 = []
+#     lista4 = []
+#     lista5 = []
